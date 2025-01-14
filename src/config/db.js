34480728f329b542dba181436f0d5799a03f0c5f
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-01-14 10:29:27 +0100

    Add database connection config using mysql2.

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+// src/config/db.js
+import mysql from 'mysql2';
+import { dbConfig } from './config.js';
+
+const pool = mysql.createPool({
+  host: dbConfig.host,
+  user: dbConfig.user,
+  password: dbConfig.password,
+  database: dbConfig.database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export default pool.promise();
