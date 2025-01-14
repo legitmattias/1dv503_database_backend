3d59f40bd9a72a35db9909588c4a38fc73f3a136
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-01-14 11:13:50 +0100

    Update config with new default password.

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,6 @@ dotenv.config()
 export const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'yourpassword',
+  password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'book_store',
 }
