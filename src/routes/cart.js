7804e73b8c1333d218fa44e5373ff0a6b69da02c
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-01-26 17:06:37 +0100

    Swap GET and POST endpoints for better structure.

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -8,22 +8,6 @@ const router = express.Router()
 // Validate session cookie for all routes.
 router.use(validateSession)
 
-// Add a book to the cart.
-router.post('/', async (req, res) => {
-  const { isbn, qty } = req.body
-  try {
-    await db.query(
-      `INSERT INTO cart (userid, isbn, qty)
-       VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE qty = qty + ?`,
-      [req.userid, isbn, qty, qty],
-    )
-    res.json({ message: 'Book added to cart' })
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: 'Failed to add to cart' })
-  }
-})
-
 // Get all items in the logged-in user’s cart.
 router.get('/', async (req, res) => {
   try {
@@ -38,6 +22,22 @@ router.get('/', async (req, res) => {
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'Failed to fetch cart items' })
+  }
+})
+
+// Add a book to the cart.
+router.post('/', async (req, res) => {
+  const { isbn, qty } = req.body
+  try {
+    await db.query(
+      `INSERT INTO cart (userid, isbn, qty)
+       VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE qty = qty + ?`,
+      [req.userid, isbn, qty, qty],
+    )
+    res.json({ message: 'Book added to cart' })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Failed to add to cart' })
   }
 })
 
