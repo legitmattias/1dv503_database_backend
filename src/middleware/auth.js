91a172c2b0e8148cfcc5b3b088b9fff9139b6d7d
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-01-26 23:22:44 +0100

    Export sessions storage and add logs.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,16 @@
-const sessions = {} // Simulated session storage.
+export const sessions = {} // Simulated session storage.
 
 export const validateSession = (req, res, next) => {
+  console.log('Validate session invoked')
+  console.log(`Stored session tokens: ${JSON.stringify(sessions, null, 2)}`)
   const sessionToken = req.headers['authorization'] || req.cookies?.sessionToken
 
   if (!sessionToken || !sessions[sessionToken]) {
+    console.log(`Session token: ${sessionToken}`)
     return res.status(401).json({ error: 'Unauthorized' })
   }
 
+  console.log(`Session token: ${sessionToken}`)
   // Attach the user ID from the session to the request object.
   req.userid = sessions[sessionToken]
   console.log('Session cookie validated. User authorized.')
